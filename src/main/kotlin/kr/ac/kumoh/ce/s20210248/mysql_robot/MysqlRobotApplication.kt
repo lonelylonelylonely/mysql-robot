3b565336bcd4da3ac001fb7f56c80b0ec2eb966b
lonelylonelylonely <mirmir730@naver.com>
2025-05-28 14:03:43 +0900

    Initial commit

---
+++ src/main/kotlin/kr/ac/kumoh/ce/s20210248/mysql_robot/MysqlRobotApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.ce.s20210248.mysql_robot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MysqlRobotApplication
+
+fun main(args: Array<String>) {
+	runApplication<MysqlRobotApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/ce/s20210248/mysql_robot/MysqlRobotApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20210248.mysql_robot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MysqlRobotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
